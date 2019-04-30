da88dc7dd4999514d6092e3da23cad6e4703d11b
ASDF44 <42999582+ASDF44@users.noreply.github.com>
2019-04-30 21:13:06 +0530

    Delete cvcv.py

--- Analysis and Derivation of Factors/cvcv.py
+++
@@ -1,9 +0,0 @@
-my_context = {
-        "my_text" : 'this is about us',
-        'this_is_true' : True,
-        'my_number': 123,
-        'my_list' : [154,287,398,467, 'abcd', 'blah']
-    }
-    
-for k,v in my_context.items():
-    print(k, '  ',v )
